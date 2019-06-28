6cf7a097d1b7a03643530e453c1b80f80b003a42
Syed Shahin Ahmed <syedshahinahmed@Syeds-MacBook-Pro.local>
2019-06-28 04:32:37 +0100

    Updates

---
+++ precache-manifest.71e8155aad9bfe763d4af7b0f5778241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfb4c2409fdf080bdd1e5f0c7cab51a",
+    "url": "/React-Ghibli-Studio--World/index.html"
+  },
+  {
+    "revision": "6f2fba0c44eefda5d1f6",
+    "url": "/React-Ghibli-Studio--World/static/css/main.70436a16.chunk.css"
+  },
+  {
+    "revision": "cea43822c912ee6adfba",
+    "url": "/React-Ghibli-Studio--World/static/js/2.4d1b9595.chunk.js"
+  },
+  {
+    "revision": "6f2fba0c44eefda5d1f6",
+    "url": "/React-Ghibli-Studio--World/static/js/main.da318b01.chunk.js"
+  },
+  {
+    "revision": "62d84ece3d6fcb358e4f",
+    "url": "/React-Ghibli-Studio--World/static/js/runtime~main.21725188.js"
+  },
+  {
+    "revision": "e86d97c08805ce75a6e83bbd1c272455",
+    "url": "/React-Ghibli-Studio--World/static/media/logo.e86d97c0.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-Ghibli-Studio--World/precache-manifest.71e8155aad9bfe763d4af7b0f5778241.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Ghibli-Studio--World/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});

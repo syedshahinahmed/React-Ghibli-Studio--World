0b0b95cf26d68e8faa1e274a65a2ea6ea2af51d8
Syed Shahin Ahmed <syedshahinahmed@Syeds-MacBook-Pro.local>
2019-06-28 04:36:04 +0100

    HashRouter Add to Roter Components

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import App from "../App";
 import Contact from "./Contact";
 
 const Router = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route path="/" component={App} exact />
       <Route path="/contact" component={Contact} />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Router;
